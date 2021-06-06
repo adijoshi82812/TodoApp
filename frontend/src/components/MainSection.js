16bda4794ea172fc59b667b7cd2f97468d1d53d4
= <=>
2021-06-06 11:56:54 +0530

    Created a boilerplate for our project

---
+++ frontend/src/components/MainSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainSection(){
+    return(
+        <main>
+            <p>This is the main section</p>
+        </main>
+    );
+}
+
+export default MainSection;
